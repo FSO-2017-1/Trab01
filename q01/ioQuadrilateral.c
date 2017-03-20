e550bd9f2d9b0d91e7410423b876cce6ca4b432c
João Henrique <joaohenrique@joaos-mbp.localdomain>
2017-03-20 19:40:10 -0300

    adding get_coordinates function

---
+++ q01/ioQuadrilateral.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "ioQuadrilateral.h"
+
+
+
+
+void get_coordinates(Quadrilateral *quadrilateral){
+  scanf("%d\n",&quadrilateral.vector1 );
+  printf("%d",quadrilateral->vector1 );
+}
--- q01/main.c
+++ q01/main.c
@@ -1,9 +1,12 @@
 #include "stdio.h"
 #include "stdlib.h"
-
+#include "ioQuadrilateral.h"
+#include "quadrilateral.h"
 
 int main(int argc, char const *argv[]) {
 
+  Quadrilateral *quadrilateral;
   printf("olá\n");
+  get_coordinates(quadrilateral);
   return 0;
 }
