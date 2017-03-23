68f2ab352dc5eb38b6e8a8cb2495f57fe2e9e4cc
Karine Valença <valenca.karine@gmail.com>
2017-03-23 16:58:47 -0300

    Change function that verify if it is convex

--- q01/libCalcQuadrilateral.c
+++ q01/libCalcQuadrilateral.c
@@ -18,15 +18,23 @@ bool is_conv_quadrilateral(Quadrilateral *quadrilateral){
 	double z[4];
 	for (i=0; i<4; i++) {
 	double dx1, dy1, dx2, dy2;
-		dx1 = quadrilateral->vector[i+2].x - quadrilateral->vector[i+1].x;
-		dy1 = quadrilateral->vector[i+2].y - quadrilateral->vector[i+1].y;
-		dx2 = quadrilateral->vector[i+1].x - quadrilateral->vector[i].x;
-		dy2 = quadrilateral->vector[i+1].y - quadrilateral->vector[i].y;
+		dx1 = quadrilateral->vector[(i+2)%4].x - quadrilateral->vector[(i+1)%4].x;
+		dy1 = quadrilateral->vector[(i+2)%4].y - quadrilateral->vector[(i+1)%4].y;
+		dx2 = quadrilateral->vector[(i+1)%4].x - quadrilateral->vector[i%4].x;
+		dy2 = quadrilateral->vector[(i+1)%4].y - quadrilateral->vector[i%4].y;
 		z[i] = (dx1 * dy2) - (dy1 * dx2);
 	}
 	printf("%lf\n", z[0]);
 	printf("%lf\n", z[1]);
 	printf("%lf\n", z[2]);
 	printf("%lf\n", z[3]);
-	return 0;
+
+	if (z[0] >= 0 && z[1] >= 0 && z[2] >= 0 && z[3] >= 0){
+		printf("Tudo positivo, convexo!");
+	}else if (z[0] <= 0 && z[1] <= 0 && z[2] <= 0 && z[3] <= 0){
+		printf("Tudo negativo, convexo");
+	}else{
+		printf("Não convexo\n");
+	}
+	return true;
 }
