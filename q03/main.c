b0ab3b3479e5525f4dd0e0416eb9159e3a961710
joaohenriquepda <joaohenrique.p.almeida@gmail.com>
2017-03-27 22:57:00 -0300

    Add question 3

---
+++ q03/main.c
@@ -0,0 +1,25 @@
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <stdio.h>
+#include "sort_list.h"
+#include "core.h"
+#include "io.h"
+#include "sort.h"
+
+int main(int argc, char *argv[]){
+
+    int check;
+
+
+    do{
+      scanf("%d\n", &check );
+      printf("%d\n",check );
+
+
+
+    }while (check != '-1');
+
+
+
+        return 0;
+}
